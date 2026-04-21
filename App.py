80f43401c46100960a18087cc0bf0a928657ad15
ambar5ingh <ambarsingh264@gmail.com>
2026-04-21 11:43:44 +0530

    Remove pip install line for plotly
    Removed pip install statement from App.py

--- App.py
+++ App.py
@@ -5,7 +5,6 @@ WRI India | Full-sector GHG emissions questionnaire with IPCC 2019 formulas
 import streamlit as st
 import numpy as np
 import pandas as pd
-pip install plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import json
